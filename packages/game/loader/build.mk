89ffb2f8140782e6af1db791bae6ea2b2e701f30
Nax <4265415+Nax@users.noreply.github.com>
2024-12-09 01:10:22 +0100

    Better makefile

--- packages/game/loader/build.mk
+++ packages/game/loader/build.mk
@@ -1,12 +1,13 @@
 # Dirs
+LOADER_DIR          := loader
 LOADER_BUILD_DIR	:= $(BUILD_DIR)/loader
 LOADER_OBJ_DIR		:= $(LOADER_BUILD_DIR)/obj
-LOADER_SRC_DIR		:= $(CURDIR)/loader/src
+LOADER_SRC_DIR		:= $(LOADER_DIR)/src
 
 # Sources and objects
 LOADER_SOURCES			:= $(shell find $(LOADER_SRC_DIR) -name '*.c' -type f)
 LOADER_OBJECTS			:= $(patsubst $(LOADER_SRC_DIR)/%.c,$(LOADER_OBJ_DIR)/%.o,$(LOADER_SOURCES))
-LOADER_LINK_SCRIPT		:= $(CURDIR)/loader/link.ld
+LOADER_LINK_SCRIPT		:= $(LOADER_DIR)/link.ld
 
 # Targets
 LOADER				:= $(LOADER_BUILD_DIR)/loader.elf
