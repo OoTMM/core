76ed0be5d0d78d6d0117a78ec5e40de839675872
Nax <4265415+Nax@users.noreply.github.com>
2024-12-10 14:25:05 +0100

    Better tree again

--- packages/game/libs/oot/build.mk
+++ packages/game/libs/oot/build.mk
@@ -22,3 +22,51 @@ else
   OOT_SOUNDFONT_EXTRACT_DIRS :=
   OOT_SEQUENCE_EXTRACT_DIRS :=
 endif
+
+ifneq ($(wildcard libs/oot/assets/audio/samples),)
+  OOT_SAMPLE_DIRS := $(shell find libs/oot/assets/audio/samples -type d)
+else
+  OOT_SAMPLE_DIRS :=
+endif
+
+ifneq ($(wildcard libs/oot/assets/audio/samplebanks),)
+  OOT_SAMPLEBANK_DIRS := $(shell find libs/oot/assets/audio/samplebanks -type d)
+else
+  OOT_SAMPLEBANK_DIRS :=
+endif
+
+ifneq ($(wildcard libs/oot/assets/audio/soundfonts),)
+  OOT_SOUNDFONT_DIRS := $(shell find libs/oot/assets/audio/soundfonts -type d)
+else
+  OOT_SOUNDFONT_DIRS :=
+endif
+
+ifneq ($(wildcard libs/oot/assets/audio/sequences),)
+  OOT_SEQUENCE_DIRS := $(shell find libs/oot/assets/audio/sequences -type d)
+else
+  OOT_SEQUENCE_DIRS :=
+endif
+
+OOT_SAMPLE_FILES         := $(foreach dir,$(OOT_SAMPLE_DIRS),$(wildcard $(dir)/*.wav))
+OOT_SAMPLE_EXTRACT_FILES := $(foreach dir,$(OOT_SAMPLE_EXTRACT_DIRS),$(wildcard $(dir)/*.wav))
+OOT_AIFC_FILES           := $(foreach f,$(OOT_SAMPLE_FILES),$(BUILD_DIR)/$(f:.wav=.aifc)) $(foreach f,$(OOT_SAMPLE_EXTRACT_FILES:.wav=.aifc),$(f:$(OOT_EXTRACTED_DIR)/%=$(BUILD_DIR)/%))
+
+OOT_SAMPLEBANK_XMLS         := $(foreach dir,$(OOT_SAMPLEBANK_DIRS),$(wildcard $(dir)/*.xml))
+OOT_SAMPLEBANK_EXTRACT_XMLS := $(foreach dir,$(OOT_SAMPLEBANK_EXTRACT_DIRS),$(wildcard $(dir)/*.xml))
+OOT_SAMPLEBANK_BUILD_XMLS   := $(foreach f,$(OOT_SAMPLEBANK_XMLS),$(BUILD_DIR)/$f) $(foreach f,$(OOT_SAMPLEBANK_EXTRACT_XMLS),$(f:$(OOT_EXTRACTED_DIR)/%=$(BUILD_DIR)/%))
+OOT_SAMPLEBANK_O_FILES      := $(foreach f,$(OOT_SAMPLEBANK_BUILD_XMLS),$(f:.xml=.o))
+OOT_SAMPLEBANK_DEP_FILES    := $(foreach f,$(OOT_SAMPLEBANK_O_FILES),$(f:.o=.d))
+
+OOT_SOUNDFONT_XMLS         := $(foreach dir,$(OOT_SOUNDFONT_DIRS),$(wildcard $(dir)/*.xml))
+OOT_SOUNDFONT_EXTRACT_XMLS := $(foreach dir,$(OOT_SOUNDFONT_EXTRACT_DIRS),$(wildcard $(dir)/*.xml))
+OOT_SOUNDFONT_BUILD_XMLS   := $(foreach f,$(OOT_SOUNDFONT_XMLS),$(BUILD_DIR)/$f) $(foreach f,$(OOT_SOUNDFONT_EXTRACT_XMLS),$(f:$(OOT_EXTRACTED_DIR)/%=$(BUILD_DIR)/%))
+OOT_SOUNDFONT_O_FILES      := $(foreach f,$(OOT_SOUNDFONT_BUILD_XMLS),$(f:.xml=.o))
+OOT_SOUNDFONT_HEADERS      := $(foreach f,$(OOT_SOUNDFONT_BUILD_XMLS),$(f:.xml=.h))
+OOT_SOUNDFONT_DEP_FILES    := $(foreach f,$(OOT_SOUNDFONT_O_FILES),$(f:.o=.d))
+
+OOT_SEQUENCE_FILES         := $(foreach dir,$(OOT_SEQUENCE_DIRS),$(wildcard $(dir)/*.seq))
+OOT_SEQUENCE_EXTRACT_FILES := $(foreach dir,$(OOT_SEQUENCE_EXTRACT_DIRS),$(wildcard $(dir)/*.seq))
+OOT_SEQUENCE_O_FILES       := $(foreach f,$(OOT_SEQUENCE_FILES),$(BUILD_DIR)/$(f:libs/%.seq=%.o)) $(foreach f,$(OOT_SEQUENCE_EXTRACT_FILES:.seq=.o),$(f:$(OOT_EXTRACTED_DIR)/%=$(BUILD_DIR)/%))
+OOT_SEQUENCE_DEP_FILES     := $(foreach f,$(OOT_SEQUENCE_O_FILES),$(f:.o=.d))
+
+OOT_SEQUENCE_TABLE := include/tables/sequence_table.h
