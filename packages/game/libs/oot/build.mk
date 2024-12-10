6d62c850ed3d276985ba0871d27cdf7e815723d2
Nax <4265415+Nax@users.noreply.github.com>
2024-12-10 13:31:38 +0100

    WIP tree again

--- packages/game/libs/oot/build.mk
+++ packages/game/libs/oot/build.mk
@@ -10,3 +10,15 @@ OOT_LDSCRIPT	:= $(OOT_ROM:.z64=.ld)
 # description of ROM segments
 OOT_SPEC := libs/oot/spec
 OOT_SRC_DIRS := $(shell find libs/oot/src -type d)
+
+ifneq ($(wildcard $(OOT_EXTRACTED_DIR)/assets/audio),)
+  OOT_SAMPLE_EXTRACT_DIRS := $(shell find $(OOT_EXTRACTED_DIR)/assets/audio/samples -type d)
+  OOT_SAMPLEBANK_EXTRACT_DIRS := $(shell find $(OOT_EXTRACTED_DIR)/assets/audio/samplebanks -type d)
+  OOT_SOUNDFONT_EXTRACT_DIRS := $(shell find $(OOT_EXTRACTED_DIR)/assets/audio/soundfonts -type d)
+  OOT_SEQUENCE_EXTRACT_DIRS := $(shell find $(OOT_EXTRACTED_DIR)/assets/audio/sequences -type d)
+else
+  OOT_SAMPLE_EXTRACT_DIRS :=
+  OOT_SAMPLEBANK_EXTRACT_DIRS :=
+  OOT_SOUNDFONT_EXTRACT_DIRS :=
+  OOT_SEQUENCE_EXTRACT_DIRS :=
+endif
